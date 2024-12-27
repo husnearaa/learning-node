14483c335b5d365353bebdc16302dcdbe05bbe6d
husnearaa <hosnearapopy462@gmail.com>
2024-12-27 13:31:19 +0600

    has added summery of nodeJS some important topic

---
+++ SummeryOfNodeJS.js
@@ -0,0 +1,8 @@
+/****
+ * 
+ * in this nodeJS topic then are some topic which is important for the developers.
+ * like as:
+ * even-driven, creating server, stream-buffer, how to handle a file in a syncronous-asyncronoyas way, how to works thread,.
+ * this concept is important. 
+ * 
+ */
--- stream-buffer.js
+++ stream-buffer.js
@@ -1,40 +1,34 @@
-const http = require('node:http');
-const fs = require('fs')
-
+const http = require("node:http");
+const fs = require("fs");
 
 //creating a server using raw node.js
-const server = http.createServer()
-
-
+const server = http.createServer();
 
 // Listener
 
-server.on('request', (req, res) =>{
-    if(req.url === '/read-file' && req.method === 'GET');
+server.on("request", (req, res) => {
+  if (req.url === "/read-file" && req.method === "GET");
 
+  // streaming file reading
+  const readableStream = fs.createReadStream(process.cwd() + "/texts/read.txt");
 
-    // streaming file reading
-    const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt')
+  readableStream.on("data", (buffer) => {
+    res.statusCode = 200;
+    res.write(buffer);
+  });
 
-    readableStream.on('data', (buffer) => {
-        res.statusCode = 200;
-        res.write(buffer)
-    })
+  readableStream.on("end", () => {
+    res.statusCode = 200;
+    res.end("The data streaming is over !");
+  });
 
-    readableStream.on('end', () => {
-        res.statusCode = 200;
-        res.end( "The data streaming is over !")
-    })
-
-    readableStream.on('error', (error) => {
-        console.log(error);
-        res.statusCode = 500;
-        res.end('Something went wrong!')
-    })
-
-})
+  readableStream.on("error", (error) => {
+    console.log(error);
+    res.statusCode = 500;
+    res.end("Something went wrong!");
+  });
+});
 
 server.listen(5000, () => {
-console.log(`server is listening on port 5000`);
-})
-
+  console.log(`server is listening on port 5000`);
+});
