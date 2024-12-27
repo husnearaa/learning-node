3072b2342875e28cdb33028c72cea18ff5edc913
husnearaa <hosnearapopy462@gmail.com>
2024-12-27 10:44:24 +0600

    has streamed and buffter to read data

--- stream-buffer.js
+++ stream-buffer.js
@@ -1,4 +1,6 @@
 const http = require('node:http');
+const fs = require('fs')
+
 
 //creating a server using raw node.js
 const server = http.createServer()
@@ -6,11 +8,25 @@ const server = http.createServer()
 
 
 // Listener
+
 server.on('request', (req, res) =>{
-    console.log(req.url, req.method);
-    res.end( "Hello world from my new server !")
+    if(req.url === '/read-file' && req.method === 'GET');
+
+
+    // streaming file reading
+    const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt')
+
+    readableStream.on('data', (buffer) => {
+        res.write(buffer)
+    })
+
+    readableStream.on('end', () => {
+        res.end( "Streaming is over !")
+    })
+
 })
 
 server.listen(5000, () => {
 console.log(`server is listening on port 5000`);
 })
+
